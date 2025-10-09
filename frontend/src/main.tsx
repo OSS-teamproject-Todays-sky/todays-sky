1e1c3a1e86292dd71b9f189d0fb00a325e268456
seongje973 <seongje973@naver.com>
2025-10-10 00:28:51 +0900

    feature: 프론트엔드 환경 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // 1. import
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+
+// './App.vue'가 아닌 './App.tsx'를 불러옵니다.
+import App from './App.tsx' 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* 2. <BrowserRouter>로 <App /> 감싸기 */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
