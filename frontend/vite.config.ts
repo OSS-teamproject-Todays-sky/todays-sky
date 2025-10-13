2ceb383c90599287a8f8051bb68d67317a30d1bc
seongje973 <seongje973@naver.com>
2025-10-14 01:22:03 +0900

    refactor: 프론트엔드_환경설정_변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
+  server: {
+    proxy: {
+      // '/api'로 시작하는 요청을 Flask 서버로 전달
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
